3a2f31974b60428d0c02e21c2cb230477393b7a9
Bindesh Chauhan <chauhanbindesh@gmail.com>
2025-03-13 14:44:38 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection',
+    version= '0.0.0',
+    author= 'Bindesh Kumar chauhan',
+    author_email= 'chauhanbindesh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
