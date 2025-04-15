011bbcbec40bf43bd0dad49d1bfcdf41d60a1656
Bindesh Chauhan <chauhanbindesh@gmail.com>
2025-04-16 00:40:48 +0530

    docker file updated

--- ObjectDetection/logger/__init__.py
+++ ObjectDetection/logger/__init__.py
@@ -2,6 +2,14 @@ import logging
 import os
 from datetime import datetime
 from from_root import from_root
+import os
+
+def from_root(*paths):
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    while not os.path.exists(os.path.join(root_dir, 'ObjectDetection')) and root_dir != '/':
+        root_dir = os.path.dirname(root_dir)
+    return os.path.join(root_dir, *paths)
+log_file = from_root("logs", "training.log")
 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
